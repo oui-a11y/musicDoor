4f778cf18278b083a1533b23ee375f552f8c95d0
xinzefeng <463836070@qq.com>
2017-09-27 16:13:10 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  components: {
+  	
+  }
+  
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
