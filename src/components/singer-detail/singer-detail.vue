66bb611514cccefb34b269869528448d33d68642
xinzefeng <463836070@qq.com>
2017-10-17 17:35:04 +0800

    歌手详情  歌曲列表

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,10 +1,11 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import MusicList from 'components/music-list/music-list'
   import {getSingerDetail} from 'api/singer'
   import {createSong} from 'common/js/song'
   import {ERR_OK} from 'api/config'
@@ -14,7 +15,13 @@
     computed: {
       ...mapGetters([
         'singer'
-      ])
+      ]),
+      title() {
+        return this.singer.name;
+      },
+      bgImage(){
+        return this.singer.avatar;
+      }
     },
     data() {
       return {
@@ -33,31 +40,25 @@
           }
         })
       },
-      _normalizeSongs(list){
+      _normalizeSongs(list) {
         let ret = [];
-        list.forEach((item)=>{
+        list.forEach((item) => {
           let {musicData} = item;
-          if(musicData.songid && musicData.albummid){
+          if (musicData.songid && musicData.albummid) {
             ret.push(createSong(musicData));
           }
         });
         return ret;
       }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
-
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
 
